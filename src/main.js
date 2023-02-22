49e6e084a8ab09df78f7bdc20c39b33cb794d045
Aline Hurtado <aline_who@Alines-MacBook-Pro.local>
2023-02-21 20:18:04 -0600

    Se agregan funcionalidades de ordenanza de datos alfabéticamente y por tipo de pokemon

--- src/main.js
+++ src/main.js
@@ -1,13 +1,56 @@
 /* eslint-disable indent */
-import slicedPokemons from './data.js';
-import pokemon from './data/pokemon/pokemon.js';
+import { sortPokemon, sortReverse, typeOrder} from "./data.js";
+import data from "./data/pokemon/pokemon.js";
+/*
 
-console.log("separados en main", slicedPokemons); 
+const ordenar = document.getElementById("btn-filtrar");
+const button = document.getElementById("btn-validar");
+const inputText = document.getElementById("caja-texto");
+const container = document.getElementById("container");*/
 
-/*let button = document.getElementById('btn-validar');
-let inputText = document.getElementById('caja-texto');
-let container = document.getElementById('container');
 
-button.addEventListener('click', function (event) {
+const dataPokemons = data.pokemon.slice(0, 50);
+const pokemonCards = document.querySelector(".contenedor-central");
 
+const templateCard = (pokemones) => {
+  pokemonCards.innerHTML = "";
+  let htmlCode = ``;
+  pokemones.forEach((fiftyPokemonArray) => {
+    htmlCode += `
+      <div class="card">
+      <img src="${fiftyPokemonArray.img}"> 
+      <div class="contenido">
+        <h3>${fiftyPokemonArray.name}</h3>
+        <p>Número: ${fiftyPokemonArray.num} </p>
+          <p>Tipo: ${fiftyPokemonArray.type}</p>
+         <p>Resistencia: ${fiftyPokemonArray.resistant}</p>
+          <p>Debilidades: ${fiftyPokemonArray.weaknesses}</p>
+        </p>
+      </div>
+    </div>`;
+  });
+  pokemonCards.innerHTML = htmlCode;
+};
+templateCard(dataPokemons);
+
+const sortingList = document.querySelector("#selectSort");
+sortingList.addEventListener("change", (e) => {
+  const valueOption = e.target.value;
+  if (valueOption === "1") {
+    const dataSortAZ = sortPokemon(dataPokemons);
+    templateCard(dataSortAZ);
+  }
+  if (valueOption === "2") {
+    const dataSortZA = sortReverse(dataPokemons);
+    templateCard(dataSortZA);
+  }
+  if (valueOption === "3") {
+    const dataSortType = typeOrder(dataPokemons);
+    templateCard(dataSortType);
+  }
+});
+/*document.getElementById("btn-sort").addEventListener("click", function () {
+    console.log("sorteados", sortPokemon(dataPokemons));
+    const dataSort = sortPokemon(dataPokemons);
+    templateCard(dataSort);
 });*/
