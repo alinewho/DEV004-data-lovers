4c7e0fc6bbd83457248f91279642bfb0c2efbbbb
Aline Hurtado <aline_who@Alines-MacBook-Pro.local>
2023-03-01 15:08:37 -0600

    Updates

--- data.js
+++ data.js
@@ -1,13 +1,10 @@
-// estas funciones son de ejemplo
-//console.log("si está agarrando toda la data de pokemones",dataPokemon);
-
 /*const slicedPokemons = Object.fromEntries(   //Función que separa el objeto tipo array, elegimos 50 pokemones
   Object.entries(dataPokemon.pokemon).slice(0, 50)
 );*/
 export const sortPokemon = (data) => {
   const sortedPokemons = data.sort( (a, b) => {
     return a.name.localeCompare(b.name);
-    /*if (a.name > b.name) {
+    /*if (a.name > b.name) {   //use later
       return 1; 
     }
     /*if (a.name > b.name) {
@@ -19,26 +16,55 @@ export const sortPokemon = (data) => {
 
 export const sortReverse = (data) => {
   const sortedZA = data.sort(function (a, b) {
-    if (a.name > b.name) {
+    return b.name.localeCompare(a.name);
+    /* if (a.name > b.name) {
       return -1;
     }
     if (a.name < b.name) {
       return 1;
     }
-    return 0;
+    return 0;*/
   });
   return sortedZA;
 } 
 
-export const typeOrder = (data) => {
-  const orderByType = data.sort(function (a, b) {
-    if (a.type < b.type) {
-      return -1;
-    }
-    if (a.type > b.type) {
-      return 1;
-    }
-    return 0;
-  });
-  return orderByType;
+export const filterType = (data, typeInPokemon) => {
+  const typesOfPokemon = data.filter(types => types.type.includes(typeInPokemon));
+  return typesOfPokemon;
 }
+ 
+export const filteredData =  (data, inputText) => {
+  inputText = inputText.trim().toLowerCase()
+  const filtrating = data.filter(names => names.name.includes(inputText))
+  return filtrating
+}
+
+export const averageData = (data) => {
+  const baseAttackValues = (data.map(a => a.stats['base-attack']));
+  const baseConverted = baseAttackValues.map(function(str) {
+    return parseInt(str);
+  });
+  const sum = baseConverted.reduce((acc, n) => (acc += n, acc), 0);
+  const promAttack = sum/baseAttackValues.length;
+  //const rounded = promAttack.toFixed(2);
+  return promAttack
+}
+
+
+/*for (let index = 0; index < data.length-1; index++) {
+    const a = Number(data[index].size.height.slice(0, data[index].size.height.length-1));
+    const b = Number(data[index+1].size.height.slice(0, data[index+1].size.height.length-1));
+    console.log(a, b);
+    inputText = Number(inputText);
+    const closest = ((a, b) => {
+      console.log((Math.abs(b - inputText)  < Math.abs(a - inputText)) ? b : a);
+      return (Math.abs(b - inputText) < Math.abs(a - inputText)) ? b : a;
+    });
+    return closest(a, b);
+  }
+
+/*const closest = data.size.height.reduce((a, b) => {
+    return (Math.abs(b - inputText) < Math.abs(a - inputText) ? b : a);
+  });
+  console.log(closest);
+  //return closest;*/
--- main.js
+++ main.js
@@ -1,21 +1,18 @@
 /* eslint-disable indent */
-import { sortPokemon, sortReverse, typeOrder} from "./data.js";
+import { sortPokemon, sortReverse, filterType, filteredData, averageData } from "./data.js";
 import data from "./data/pokemon/pokemon.js";
-
-/*l
-
-button.addEventListener('click', function (event) {
-
-});*/
+/*
 
 const ordenar = document.getElementById("btn-filtrar");
 const button = document.getElementById("btn-validar");
-const inputText = document.getElementById("caja-texto");
-const container = document.getElementById("container");
+const container = document.getElementById("container");*/
 
+const dataPokemons = data.pokemon.slice(0, 250);
+const pokemonCards = document.querySelector(".cardContainer");
+const errorContainer = document.querySelector(".error");
+//const operationContainer = document.getElementById("operation");
+//const constContainer = document.querySelector(".resultContainer");
 
-const dataPokemons = data.pokemon.slice(0, 50);
-const pokemonCards = document.querySelector(".contenedor-central");
 
 const templateCard = (pokemones) => {
   pokemonCards.innerHTML = "";
@@ -28,11 +25,12 @@ const templateCard = (pokemones) => {
         <h3>${fiftyPokemonArray.name}</h3>
         <p>Número: ${fiftyPokemonArray.num} </p>
           <p>Tipo: ${fiftyPokemonArray.type}</p>
-         <p>Resistencia: ${fiftyPokemonArray.resistant}</p>
-          <p>Debilidades: ${fiftyPokemonArray.weaknesses}</p>
+         <p>Altura: ${fiftyPokemonArray.size.height}</p>
+          <p>Peso: ${fiftyPokemonArray.size.weight}</p>
         </p>
       </div>
     </div>`;
+    
   });
   pokemonCards.innerHTML = htmlCode;
 };
@@ -49,13 +47,75 @@ sortingList.addEventListener("change", (e) => {
     const dataSortZA = sortReverse(dataPokemons);
     templateCard(dataSortZA);
   }
-  if (valueOption === "3") {
-    const dataSortType = typeOrder(dataPokemons);
-    templateCard(dataSortType);
+});
+
+
+const templatePromedio = (data) => {
+  errorContainer.innerHTML = "";
+  let htmlCode = ``;
+ 
+    htmlCode += `
+    <p class="averageBox averageText"> ¿Sabías que? &#128269	
+    <br>
+    En promedio el ataque de este tipo de pokemones es de ${data} <p>  
+    `;
+  errorContainer.innerHTML = htmlCode;
+};
+
+const sortingType = document.querySelector("#selectType");
+sortingType.addEventListener("change", (b) => {
+  const valueOptionType = b.target.value;
+  const poisonType = filterType(dataPokemons, valueOptionType);
+  templateCard(poisonType);
+  console.log("poison Function",poisonType)
+  const finalAverage = averageData(poisonType);
+  templatePromedio(finalAverage);
+});
+
+
+
+const inputValue = document.getElementById("inputTextBox");
+inputValue.addEventListener("input", (g) => {
+  const valueInputText = g.target.value;
+  const filterAndData = filteredData(dataPokemons, valueInputText);
+  if (valueInputText.length > 0) {
+    errorContainer.innerHTML = "";
+    if (filterAndData.length === 0) {
+      const htmlCode = `
+    <p class="noResult errorText">No se encontró ningún pokémon que coincida con la búsqueda
+    <br> Intenta con otro nombre<p>
+    `;
+    errorContainer.innerHTML = htmlCode;
+    }
+    if (filterAndData.length > 0) {
+      templateCard(filterAndData);
+    }
   }
 });
+
+const returnButton = document.getElementById("clear");
+returnButton.addEventListener("click", () => {
+  //función que va a borrar la búsqueda y regresarnos
+  errorContainer.innerHTML = "";
+  templateCard(dataPokemons);
+});
+
+/*const operationSection = document.querySelector(".btnOperation");
+operationSection.addEventListener("click", function () {
+  const htmlCodeOperation = `
+  <br>
+  <input placeholder= "Ingresa tu altura en metros" class="inputOperation" id="inputOperation"></input>
+  `;
+  operationContainer.innerHTML = htmlCodeOperation;
+  const inputTextOperation = document.getElementById("inputOperation");
+  inputTextOperation.addEventListener("input", (h) => {
+    const valueInputOperation = h.target.value;
+    const computedData = heightOperation(dataPokemons, valueInputOperation);
+    //  templateCard(computedData);
+  })
+});
+
 /*document.getElementById("btn-sort").addEventListener("click", function () {
-    console.log("sorteados", sortPokemon(dataPokemons));
     const dataSort = sortPokemon(dataPokemons);
     templateCard(dataSort);
 });*/
